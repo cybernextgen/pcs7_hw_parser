c3a19c76edc85f92ccb48484fd9e404504596a38
Инженер Петров <cybernextgen@gmail.com>
2020-12-23 23:48:53 +0800

    Refactoring

--- pcs7_hw_parser/app.py
+++ pcs7_hw_parser/app.py
@@ -2,34 +2,27 @@ import os
 import argparse
 import logging
 import settings
-from pydoc import locate
+import pydoc
 
 
-def dynamic_import(class_path):
-    return locate(class_path)
+def import_plugins(plugin_package, plugin_modules):
+    plugins = []
+    for plugin_path in plugin_modules:
+        plugin_class = pydoc.locate('{}.{}'.format(plugin_package, plugin_path))
+        if not plugin_class:
+            logging.warning('Plugin "{}" not found'.format(plugin_class))
+        else:
+            plugins.append(plugin_class)
+    return plugins
 
 
 def main():
     parent_args_parser = argparse.ArgumentParser(add_help=False)
 
-    parsers_pool = []
-    serializers_pool = []
-
     result = {}
 
-    for parser_path in settings.PARSERS_AVAILABLE:
-        parser_class = dynamic_import('parsers.{}'.format(parser_path))
-        if not parser_class:
-            logging.warning('Parser "{}" not found'.format(parser_class))
-        else:
-            parsers_pool.append(parser_class(result))
-
-    for serializer_path in settings.SERIALIZERS_AVAILABLE:
-        serializer_class = dynamic_import('serializers.{}'.format(serializer_path))
-        if not serializer_class:
-            logging.warning('Serializer "{}" not found'.format(serializer_class))
-        else:
-            serializers_pool.append(serializer_class(parent_args_parser))
+    parsers_pool = [cl(result) for cl in import_plugins('parsers', settings.PARSERS_AVAILABLE)]
+    serializers_pool = [cl(parent_args_parser) for cl in import_plugins('serializers', settings.SERIALIZERS_AVAILABLE)]
 
     # json_serializer.JSONSerializer(parent_parser)
     print(vars(parent_args_parser.parse_args()))
